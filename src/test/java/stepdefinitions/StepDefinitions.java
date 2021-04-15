a8c0e7fe77f01124b6d641c8c64a49686f25a06e
davs88 <david.svanhammar@hotmail.com>
2021-04-15 11:12:51 +0200

    new project

---
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -0,0 +1,12 @@
+package stepdefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertEquals;
+public class StepDefinitions {
+}
