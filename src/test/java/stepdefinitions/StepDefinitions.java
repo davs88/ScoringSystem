4573c90ba1b4b5013746dec358462277c87f247b
davs88 <david.svanhammar@hotmail.com>
2021-04-19 10:59:12 +0200

    is this working.

--- src/test/java/stepdefinitions/StepDefinitions.java
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -9,7 +9,35 @@ import javax.swing.*;
 
 import static org.junit.Assert.assertEquals;
 public class StepDefinitions {
-    {
-        System.out.println("hej allihopa");
+
+
+
+
+        @Given("I want to enter a {int}")
+        public void i_want_to_enter_a (Integer int1){
+
+
     }
-}
+
+        @When("i press enter")
+        public void i_press_enter () {
+
+
+
+
+    }
+
+        @Then("the result {int} should be on the screen")
+        public void the_result_should_be_on_the_screen (Integer int1){
+
+
+    }
+    }
+
+
+
+
+
+
+
+
